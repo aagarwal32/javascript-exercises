bfbe49aa91496f610e94d75ffb9fe71dd53f88bb
Arjun <arjunagw02@gmail.com>
2024-08-03 19:25:29 -0400

    completed javascript exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elems_to_remove) {
+    const newArray = []
+    arr.forEach((elem) => {
+        if (!elems_to_remove.includes(elem)) {
+            newArray.push(elem); 
+        }
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    let result=0;
 
+    for (let num of args){
+        if(num < 0 || !Number.isInteger(num)) {return "ERROR";}
+    }
+
+    min = Math.min(...args);
+    max = Math.max(...args);
+    for(let i=min; i<=max; i++) {result += i;}
+
+    return result;
 };
 
 // Do not edit below this line
